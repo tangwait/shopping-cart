b9d9ca9d8dde38d8df96071f55764e68da486ffb
tangwai <tmichael11314@gmail.com>
2024-06-09 18:40:51 -0500

    add product images add products components import products to shop

--- shopping-cart/src/Shop.jsx
+++ shopping-cart/src/Shop.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Link } from "react-router-dom";
+import Products from './components/Products'
 
 
 
@@ -7,9 +8,20 @@ function Shop() {
 
   return (
     <>
-      <h1>Shop clothes and find amazing deals</h1>
-      <Link to="/" className="button-link">Home</Link>
-      <Link to="/cart" className="button-link">Cart</Link>
+    <div className='header'>
+      <div className='title'>
+        <h1>Shop clothes and find amazing deals</h1>
+      </div>
+      <div className='links'>
+        <Link to="/" className="button-link">Home</Link>
+        <Link to="/cart" className="button-link">Cart</Link>
+      </div>
+    </div>
+    <div className='shop'>
+      <div className='shop-body'>
+        <Products/>
+      </div>
+    </div>
     </>
   )
 }
---
+++ shopping-cart/src/components/Products.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setProducts(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+            <li key={product.id}>
+                <div>
+                    {product.title}
+                </div>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
