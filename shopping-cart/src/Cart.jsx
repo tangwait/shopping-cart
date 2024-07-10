187a750f0a2d532d72e69cd03d2411b4a1eccf7a
tangwai <tmichael11314@gmail.com>
2024-07-10 14:53:18 -0500

    grab item title/quant/price

--- shopping-cart/src/Cart.jsx
+++ shopping-cart/src/Cart.jsx
@@ -1,13 +1,30 @@
+
 import { Link } from "react-router-dom";
+import { useShoppingCart } from './components/useShoppingCart';
 
 const Cart = () => {
-    return (
-        <>
-        <h1>Shopping Cart</h1>
-        <Link to="/" className="button-link">Home</Link>
-        <Link to="/shop" className="button-link">Shop</Link>
-        </>
-    )
-}
+  const { cart, removeFromCart } = useShoppingCart();
 
-export default Cart
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      <Link to="/" className="button-link">Home</Link>
+      <Link to="/shop" className="button-link">Shop</Link>
+      <div className='cart'>
+        <ul>
+          {cart.map(item => (
+            <li key={item.id}>
+              <img src={item.imageUrl} alt={item.title} style={{ width: '100px', height: '100px' }} />
+              <p className='cart-item-title'>{item.title}</p>
+              <p className='cart-item-quantity'>Quantity: {item.quantity}</p>
+              <p className='cart-item-price'>Price: ${item.price}</p>
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Cart;
